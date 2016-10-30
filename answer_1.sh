69b89b8f56f94e227b790366f9199a55915309b6
Conor Flynn <flynncp@linux2.cs.tamu.edu>
2016-10-29 19:43:16 -0500

    Nevermind. No idea how testing isn't working

--- answer_1.sh
+++ answer_1.sh
@@ -2,12 +2,12 @@
 
 target="$1"
 
-sol 		="./solutions/dcc"
-sol_output 	="11.asm"
-spim_output ="11.out"
+sol="./solutions/dcc"
+sol_output="11.asm"
+spim_output="11.out"
 
 rm -f $sol_output
 rm -f $spim_output
 
-$sol < $target > $sol_output
+$sol < $target > $sol_output 2>&1
 spim -file $sol_output > $spim_output 2>&1
--- answer_2.sh
+++ answer_2.sh
@@ -2,12 +2,12 @@
 
 target="$1"
 
-sol 		="./dcc"
-sol_output 	="22.asm"
-spim_output ="22.out"
+sol="./dcc"
+sol_output="22.asm"
+spim_output="22.out"
 
 rm -f $sol_output
 rm -f $spim_output
 
-$sol < $target > $sol_output 
+$sol < $target > $sol_output 2>&1
 spim -file $sol_output > $spim_output 2>&1
